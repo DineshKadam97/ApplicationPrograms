3f91bed8ce37ed9b7aaca04d308fd71fd2ecfda8
DineshKadam97 <132736142+DineshKadam97@users.noreply.github.com>
2023-06-19 12:33:25 +0530

    Add files via upload

---
+++ Complex Application Programs/program182.c
@@ -0,0 +1,36 @@
+// Toggle
+
+#include<stdio.h>
+
+void strtoggleX(char *str)
+{
+    int Gap = 'a' - 'A';        //If you dont know the gap 
+
+    while(*str != '\0')
+    {
+        if((*str >= 'a') && (*str <= 'z'))
+        {
+            *str = *str - Gap;
+        }
+        else if((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + Gap;
+        }
+        
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    strtoggleX(Arr);
+
+    printf("String after editing is : %s\n",Arr);
+
+    return 0;
+}
